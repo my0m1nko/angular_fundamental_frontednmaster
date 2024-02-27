b88ed64880c658bf7ecb9f7030dc5e16e099d283
my0m1nko <myominkomrs.dev@gmail.com>
2024-02-27 20:34:30 +0630

    siginal update

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports:[UserInfoComponent],
+  template: `
+  <h1>User Listing</h1>
+  @for (user of userDatas; track user.id) {
+    <app-user-info [user]="user"/>
+  }
+  `,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  userService = inject(UserService);
+  userDatas: User[] =[];
+  constructor(private userData : UserService) {
+
+  }
+  async ngOnInit() {
+    await this.userService.getUserData().then(
+      data =>{
+        this.userDatas = data;
+      }
+    )
+  }
+
 }
